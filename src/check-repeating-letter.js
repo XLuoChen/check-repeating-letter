2f9fe459bd76bb40c9087df59cb2696ba4a6a8e9
XLuoChen <1425145741@qq.com>
2017-03-05 14:45:35 +0800

    add answer

--- src/check-repeating-letter.js
+++ src/check-repeating-letter.js
@@ -1,6 +1,13 @@
 function checkRepeatingLetter(str) {
   //在这里写入代码
-  
+  const array = str.split('');
+  for (let i = 0; i < array.length; i++) {
+    if (array.lastIndexOf(array[i]) !== i) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 module.exports = checkRepeatingLetter;
